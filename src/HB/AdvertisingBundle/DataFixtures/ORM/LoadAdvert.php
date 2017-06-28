67332da4004c37a62c6f3d8e67f294cd90602d63
FP <developpement.fabrice@protonmail.ch>
2017-06-28 14:53:28 +0200

    Add adverts fixtures

---
+++ src/HB/AdvertisingBundle/DataFixtures/ORM/LoadAdvert.php
@@ -0,0 +1,50 @@
+<?php
+namespace HB\Advertising\DataFixtures\ORM;
+
+use HB\AdvertisingBundle\Entity\Advert;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * Created by PhpStorm.
+ * User: fabrice
+ * Date: 13/06/17
+ * Time: 18:07
+ */
+//php bin/console doctrine:fixtures:load --fixtures=src/HB/AdvertisingBundle/DataFixtures/ORM
+class LoadAdvert implements FixtureInterface {
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $title = [
+            'Développeur Symfony',
+            'Développeur AngularJS',
+            'Développeur JS',
+            'Développeur Laravel',
+        ];
+        $nbAdvert= count($title);
+
+        $content = [
+            '1- On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L\'avantage du Lorem Ipsum sur un texte générique comme \'Du texte. Du texte. Du texte.\' est qu\'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour \'Lorem Ipsum\' vous conduira vers de nombreux sites qui n\'en sont encore qu\'à leur phase de construction',
+            '2- On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L\'avantage du Lorem Ipsum sur un texte générique comme \'Du texte. Du texte. Du texte.\' est qu\'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour \'Lorem Ipsum\' vous conduira vers de nombreux sites qui n\'en sont encore qu\'à leur phase de construction',
+            '3- On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L\'avantage du Lorem Ipsum sur un texte générique comme \'Du texte. Du texte. Du texte.\' est qu\'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour \'Lorem Ipsum\' vous conduira vers de nombreux sites qui n\'en sont encore qu\'à leur phase de construction',
+            '4- On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L\'avantage du Lorem Ipsum sur un texte générique comme \'Du texte. Du texte. Du texte.\' est qu\'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour \'Lorem Ipsum\' vous conduira vers de nombreux sites qui n\'en sont encore qu\'à leur phase de construction',
+            ];
+
+        for ($item = 0 ; $item < $nbAdvert ; $item++) {
+            $advert = new Advert();
+
+            // ADVERT /////////////////////////////////////////////////////////
+            $advert->setTitle($title[$item]);
+            $advert->setContent($content[$item]);
+
+            $manager->persist($advert);
+
+            unset($advert);
+        }
+        $manager->flush();
+    }
+}
