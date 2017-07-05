7279bfdf963841e079eff48563c5715e9aae3abf
FP <developpement.fabrice@protonmail.ch>
2017-07-05 15:48:27 +0200

    installation of twig extensions

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -21,6 +21,7 @@ class ComposerStaticInitc861a7a2eeb2b3bef9a92fc1cf364822
     public static $prefixLengthsPsr4 = array (
         'T' => 
         array (
+            'Twig\\Extensions\\' => 16,
             'Twig\\' => 5,
             'Tests\\' => 6,
         ),
@@ -90,6 +91,10 @@ class ComposerStaticInitc861a7a2eeb2b3bef9a92fc1cf364822
     );
 
     public static $prefixDirsPsr4 = array (
+        'Twig\\Extensions\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/twig/extensions/src',
+        ),
         'Twig\\' => 
         array (
             0 => __DIR__ . '/..' . '/twig/twig/src',
@@ -251,6 +256,10 @@ class ComposerStaticInitc861a7a2eeb2b3bef9a92fc1cf364822
     public static $prefixesPsr0 = array (
         'T' => 
         array (
+            'Twig_Extensions_' => 
+            array (
+                0 => __DIR__ . '/..' . '/twig/extensions/lib',
+            ),
             'Twig_' => 
             array (
                 0 => __DIR__ . '/..' . '/twig/twig/lib',
